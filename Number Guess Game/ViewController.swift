aff8f3933f71bb099580cbc056e218760d9b474b
Abdulkaiyum Shaikh <abdulkaiyum.creatrix>
2020-02-17 16:54:47 -0500

    Initial Commit

---
+++ Number Guess Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Number Guess Game
+//
+//  Created by Student on 2020-02-17.
+//  Copyright © 2020 Kaiyum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Number Guess GameTests/Number_Guess_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Number_Guess_GameTests.swift
+//  Number Guess GameTests
+//
+//  Created by Student on 2020-02-17.
+//  Copyright © 2020 Kaiyum. All rights reserved.
+//
+
+import XCTest
+@testable import Number_Guess_Game
+
+class Number_Guess_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
